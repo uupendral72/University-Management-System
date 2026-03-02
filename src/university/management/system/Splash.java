5ecfa3b4d6e943db8688e31aff02877df54453e3
uupendral72 <uupendral72@gmail.com>
2026-03-02 13:50:32 +0530

    Initial commit

--- src/university/management/system/Splash.java
+++ src/university/management/system/Splash.java
@@ -36,6 +36,8 @@ public class Splash extends JFrame implements Runnable
             {
                 Thread.sleep(7000);
                 setVisible(false);
+                
+                new Login();
             }
             catch(Exception e)
             { }
